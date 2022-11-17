9741fd1fcde1c9e6b375e26fb3c7db09c93ca5f0
Vinayak Jaiwant <vjaiwantx@gmail.com>
2022-11-17 11:58:53 +0530

    Basic CRUD ops for product_details and product_info completed

--- db/main.py
+++ db/main.py
@@ -7,7 +7,9 @@ class PandaDB:
             self.metadata = db.MetaData()
         except:
             print("Some Error has Occured")
+    # Functions for Product Details Table:
 
+    # Not neccessary but useful during tests
     def SelectAllFromProductDetails(self):
         try:
             ResultSet = []
@@ -86,11 +88,81 @@ class PandaDB:
                 self.connection.execute(query)
                 print("Row Deleted Successfully")
                 return True 
+    
+    # Product Info Page
+
+    def SelectAllfromProductInfo(self):
+        try:
+            ResultSet = []
+            product_info = self.getProductInfo()
+            query = db.select([product_info])
+            result = self.connection.execute(query)
+            for i in result:
+                ResultSet.append(i._asdict())
+            return ResultSet
+        except:
+            print("Some Error Occured")
+            return False
+
+    def SelectRowsBySameProductID(self,product_id):
+        try:
+            ResultSet = []
+            product_info = self.getProductInfo()
+            query = db.select([product_info]).where(product_info.c.product_id == product_id)
+            result = self.connection.execute(query)
+            for row in result:
+                ResultSet.append(row._asdict())
+            return ResultSet
+        except:
+            print("Some Error Occured")
+            return False 
+
+    def InsertItemintoProductInfo(self,product_id,size,color,quantity,discount,price):
+        try:
+            product_info = self.getProductInfo()
+            item = {'product_id' : product_id, 'size' : size, 'color' : color}
+            items = self.SelectRowsBySameProductID(product_id=product_id)
+            filtered_item = [x for x in items if x['size'] == size and x['color'] == color]
+            if(len(items) == 0 or len(filtered_item) == 0):
+                query = db.insert(product_info).values(product_id=product_id,size=size,color=color,quantity=quantity,discount=discount,price=price)
+                self.connection.execute(query)
+                print("Row Inserted Successfully")
+                return True
+            else:
+                item_id = filtered_item[0]["item_id"]
+                original_quantity = filtered_item[0]["quantity"]
+                query = db.update(product_info).values(quantity = quantity + original_quantity).where(product_info.c.item_id == item_id)
+                self.connection.execute(query)
+                print("Row Inserted Successfully")
+                return True 
+        except:
+            print("An Error Occured")
+            return False 
+
+    # Core Functions       
+    def SelectProducts(self):
+        try:
+            ResultSet = []
+            product_info = self.getProductInfo()
+            product_details = self.getProductDetails()
+            query = db.select([product_details.c.product_name,product_details.c.product_desc,product_info.c.size,product_info.c.color,product_details.c.product_gender,product_details.c.product_category,product_info.c.quantity,product_info.c.discount,product_info.c.price])
+            query = query.select_from(product_details.join(product_info, product_details.c.product_id == product_info.c.product_id))
+            results = self.connection.execute(query)
+            for row in results:
+                ResultSet.append(row._asdict())
+            return ResultSet
+        except:
+            print("Some Error Occured")
+            return False 
 
     # Additional Functions
     def getProductDetails(self):
         product_details = db.Table('product_details', self.metadata, autoload=True, autoload_with=self.engine)
         return product_details
+
+    def getProductInfo(self):
+        product_info = db.Table('product_info',self.metadata,autoload=True, autoload_with=self.engine)
+        return product_info
 
     def viewKeysforProductDetails(self):
         try:
@@ -108,3 +180,8 @@ class PandaDB:
             return False 
         else:
             return True
+
+# x = PandaDB()
+# # x.InsertItemintoProductInfo(product_id=6,size="XL",color="Red",quantity=12,discount=5,price=500)
+# # print(x.SelectRowsBySameProductID(6))
+# print(x.SelectProducts())
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from db.main import PandaDB
 
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {"Message":"Welcome to Purple Panda Backend API"}
+# Initialising the Database Connection as well as SQLAlchemy object
+obj = PandaDB()
+
+# defining for inventory route
+inventory_api_route = "/api/inventory/"
+
+@app.get(inventory_api_route+'products')
+def product_list():
+    products = obj.SelectProducts()
+    return products
